b5ca6338a195aec4b4571cfd6e5ca969ed89b8eb
terencegaffudSU <terence.gaffud@skillsunion.com>
2025-06-26 20:58:57 +0800

    added integration with prometheus

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const app = express();
-const promBundle = require("express-prom-bundle");
-// Add the options to the prometheus middleware most option are for http_request_duration_seconds histogram metric
-const metricsMiddleware = promBundle({
- includeMethod: true, 
- includePath: true, 
- includeStatusCode: true, 
- includeUp: true,
- customLabels: {project_name: 'hello_world', project_type: 'test_metrics_labels'},
- promClient: {
- collectDefaultMetrics: {
- }
- }
+const promClient = require('prom-client');
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+// Collect default metrics
+collectDefaultMetrics();
+// Create a custom counter
+const httpRequestsTotal = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status']
 });
-// add the prometheus middleware to all routes
-app.use(metricsMiddleware)
-// default endpoint 
+
 app.get("/",(req,res) => res.json({
  "GET /": "All Routes", 
  "GET /hello": "Hello World", 
  "GET /metrics": "Metrics data",
  "POST /bye": "POST Request: + post data"
 }));
-// hello world rest endpoint 
-app.get("/hello", (req,res) => res.send("Hello World"));
+// rest endpoint 
+app.get("/hello", (req,res) => {
+    httpRequestsTotal.inc({ method: 'GET', route: '/', status: 200 });
+    res.send("Hello World")
+});
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 app.post("/bye", (req,res) => res.send("POST Request : "+ req));
 app.listen(8080, function () { 
  console.log('Listening at http://localhost:8080'); 
